79ea2e6a7a5f8c802f2f5a9956399da3ad99fb2f
Shashikant Kalaskar <shashikant.kalaskar@apptware.com>
2020-09-04 06:19:16 +0530

    Implemented login and product listing UI component

--- product-catalogue/src/app/app-routing.module.ts
+++ product-catalogue/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: ProductListingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
